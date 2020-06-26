81740e548b808789f6e4b10b9b12c2014b0f7c88
M-S-72 <s.marten72@icloud.com>
2020-06-26 14:06:28 +0200

    Spreads only after echerite has been mined and fixed some errors

--- src/main/java/de/martenschaefer/enderinvasion/SpreadUtil.java
+++ src/main/java/de/martenschaefer/enderinvasion/SpreadUtil.java
@@ -1,44 +1,18 @@
-package de.martenschaefer.enderinvasion.mixin;
+package de.martenschaefer.enderinvasion;
 
-import de.martenschaefer.enderinvasion.block.EnderInvasionBlockTags;
 import de.martenschaefer.enderinvasion.recipe.SpreadRecipe;
 import de.martenschaefer.enderinvasion.recipe.SpreadRecipeManager;
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.Difficulty;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Random;
 
-@Mixin(AbstractBlock.class)
-public class BlockMixin {
+public class SpreadUtil {
 
- @Inject(method = "randomTick", at = @At("RETURN"))
- public void randomTick(ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
-
-  if(!EnderInvasionBlockTags.END_SPREADABLE.contains(world.getBlockState(pos).getBlock())) return;
-
-  /* if (!canSurvive(state, world, pos)) {
-
-   world.setBlockState(pos, getSurviveBlock().getDefaultState());
-   return;
-  } */
-
-  Difficulty difficulty = world.getDifficulty();
-
-  for (int i = 0; i < difficulty.getId(); i++) {
-
-   BlockPos blockPos = pos.add(randomNearbyBlockPos(difficulty, random));
-   this.spreadTo(difficulty, world, pos, blockPos, random);
-  }
- }
  public static boolean spreadTo(Difficulty difficulty, ServerWorld world, BlockPos from, BlockPos to, Random random) {
 
   BlockState blockState = world.getBlockState(to);
