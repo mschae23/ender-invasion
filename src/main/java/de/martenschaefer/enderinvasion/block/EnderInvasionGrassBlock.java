7c2a7708db9a0cb6cb078c58623b10eb3cbbaa30
mschae23 <s.marten72@icloud.com>
2020-07-14 16:13:49 +0200

    Made end grass blocks spread onto end dirt

--- src/main/java/de/martenschaefer/enderinvasion/block/EnderInvasionGrassBlock.java
+++ src/main/java/de/martenschaefer/enderinvasion/block/EnderInvasionGrassBlock.java
@@ -1,5 +1,7 @@
 package de.martenschaefer.enderinvasion.block;
 
+import de.martenschaefer.enderinvasion.EnderInvasionMod;
+import de.martenschaefer.enderinvasion.State;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.GrassBlock;
@@ -36,6 +38,19 @@ public class EnderInvasionGrassBlock extends GrassBlock {
   if (!canSurvive(world, state, pos)) {
 
    world.setBlockState(pos, EnderInvasionBlocks.END_DIRT.getDefaultState());
+  } else {
+
+   if (EnderInvasionMod.STATE.get(world.getLevelProperties()).value() == State.ENDER_INVASION &&
+           world.getLightLevel(pos.up()) >= 9) {
+
+    for(int i = 0; i < 4; ++i) {
+
+     BlockPos blockPos = pos.add(random.nextInt(3) - 1, random.nextInt(5) - 3, random.nextInt(3) - 1);
+     if (world.getBlockState(blockPos).isOf(EnderInvasionBlocks.END_DIRT) && canSurvive(world, this.getDefaultState(), blockPos)) {
+      world.setBlockState(blockPos, this.getDefaultState().with(SNOWY, world.getBlockState(blockPos.up()).isOf(Blocks.SNOW)));
+     }
+    }
+   }
   }
  }
 }
