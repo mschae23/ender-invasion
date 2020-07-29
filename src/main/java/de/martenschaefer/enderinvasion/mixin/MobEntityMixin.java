5f8fc2f969b74ba8509ca41e72d2dd549abcfcee
mschae23 <s.marten72@icloud.com>
2020-07-29 21:49:57 +0200

    Endermen can't spawn before the ender invasion now

--- src/main/java/de/martenschaefer/enderinvasion/mixin/MobEntityMixin.java
+++ src/main/java/de/martenschaefer/enderinvasion/mixin/MobEntityMixin.java
@@ -1,14 +1,19 @@
 package de.martenschaefer.enderinvasion.mixin;
 
+import de.martenschaefer.enderinvasion.EnderInvasionMod;
+import de.martenschaefer.enderinvasion.State;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldView;
+import net.minecraft.world.WorldAccess;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import java.util.Random;
 
 @SuppressWarnings("unused")
 @Mixin(MobEntity.class)
@@ -18,9 +23,15 @@ public abstract class MobEntityMixin extends LivingEntity {
 
   super(entityType, world);
  }
- @Inject(method = "canSpawn(Lnet/minecraft/world/WorldView;)Z", at = @At("RETURN"))
- public void canSpawn(WorldView world, CallbackInfoReturnable cir) {
+ @Inject(method = "canMobSpawn", at = @At("RETURN"), cancellable = true)
+ private static void canMobSpawn(EntityType<? extends MobEntity> type, WorldAccess world, SpawnReason spawnReason, BlockPos pos, Random random, CallbackInfoReturnable cir) {
 
-  // TODO prevent enderman spawn before ender invasion
+  if(type == EntityType.ENDERMAN) {
+
+   if(EnderInvasionMod.STATE.get(world.getLevelProperties()).value() == State.PRE_ECHERITE) {
+
+    cir.setReturnValue(false);
+   }
+  }
  }
 }
